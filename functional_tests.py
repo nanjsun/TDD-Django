bf41bd1684bc49b5b3d05a367dcf2ebf65a06d28
nanjsun <279956327@qq.com>
2017-04-02 00:05:22 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -6,8 +6,8 @@ class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 	
-	def tearDown(self):
-		self.browser.quit()
+#	def tearDown(self):
+#		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
@@ -30,7 +30,6 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertTrue(
 			any(row.text == '1:Buy peacock feathers' for row in rows)
 			)
-
 
 		self.fail('Finish the test!')
 
